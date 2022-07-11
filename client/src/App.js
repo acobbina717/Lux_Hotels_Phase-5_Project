dd839bdb1be672295569b0441d87bcab6150bb32
Abeku <acobbina717@gmail.com>
2022-07-10 20:00:31 -0400

    restrctured rails api for user sessions

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,47 @@
 import "./App.css";
-import React, { useState, useEffect } from "react";
-import { Switch, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import Main from "./Components/Main";
+import HotelsPage from "./Components/HotelsPage";
+import HotelInfoPage from "./Components/HotelInfoPage";
+import UserProfile from "./Components/UserProfile";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [user, setUser] = useState(null);
+  const [currentUser, setCurrentUser] = useState({});
+  console.log("user", user);
+  console.log("currentUser", currentUser.username);
 
-  const hello = () => {
-    fetch("/hello")
-      .then((res) => res.json())
-      .then((data) => setCount(data.count));
+  const getCurrentUser = () => {
+    fetch("/current_user").then((res) => {
+      if (res.ok) {
+        res.json().then((data) => {
+          setCurrentUser(data);
+        });
+      }
+    });
   };
   useEffect(() => {
-    hello();
+    getCurrentUser();
   }, []);
 
   return (
-    <div className="App">
-      <Switch>
-        <Route path="/testing">
-          <h1>Hello, world!</h1>
-        </Route>
-        <Route path="/">
-          <h1>Page Count: {count}</h1>
-        </Route>
-      </Switch>
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Main user={user} setUser={setUser} getCurrentUser={getCurrentUser} />
+        }
+      />
+      <Route path="/hotels" element={<HotelsPage />} />
+      <Route path="/hotels/:id" element={<HotelInfoPage />} />
+      {currentUser ? (
+        <Route
+          path={`/${currentUser.username}`}
+          element={<UserProfile setCurrentUser={setCurrentUser} />}
+        />
+      ) : null}
+    </Routes>
   );
 }
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
+  # resources :users, only: [:create, :show]
+  get '/current_user', to: 'users#show'
+  delete '/logout', to: 'sessions#destroy'
+  post '/login', to: 'sessions#create'
+  post '/signup', to: 'users#create'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get '/hello', to: 'application#hello_world'
+  # get '/hello', to: 'application#hello_world'
 
   get '*path',
       to: 'fallback#index',
