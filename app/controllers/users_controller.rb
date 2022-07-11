adf7ce0791852aa5b3c955d6e50cd45479c40e36
Abeku <acobbina717@gmail.com>
2022-07-10 20:52:20 -0400

    app and clinet push

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+
+  def index 
+    user = User.all
+    render json: user
+  end
+
+  def show
+    user = User.find_by(id: session[:user_id])
+    if user
+      render json: user
+    else
+      render json: { error: "Not authorized" }, status: :unauthorized
+    end
+  end
+
+  def create
+    user = User.create!(user_params)
+    session[:user_id] = user.id
+    render json: user
+
+
+
+    
+  end
+
+
+
+  private
+  def user_params
+    params.permit(:username, :email, :password)
+  end
+end
---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence:true, length: { minimum: 6 }
+  validates :email, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/, presence: true, uniqueness: true
+  validate :permitted_emails
+
+    def permitted_emails
+        unless email.match?(/gmail.com|yahoo.com|icloud.com|aol.com|hotmail.com|ymail.com/)
+            errors.add(:permitted_emails, "Sorry that email is not permitted.")
+        end
+    end
+    
+end
